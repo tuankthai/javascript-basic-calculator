7678c2339534a76962a2cc4c16a4ca09eb7fcd26
tuankthai <ttlive4jc@gmail.com>
2023-03-27 20:41:36 -0700

    workaround

---
+++ test.js
@@ -0,0 +1,38 @@
+function calculate(operation) {
+    const input1 = document.getElementById("input1").value;
+    const input2 = document.getElementById("input2").value;
+    let result;
+    console.log(typeof (input1));
+
+    if (input1 === "" || input2 === "") {
+        alert("Please enter both numbers.");
+        return;
+    }
+
+    const num1 = parseFloat(input1);
+    const num2 = parseFloat(input2);
+
+    switch (operation) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (num2 === 0) {
+                alert("Cannot divide by zero.");
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            alert("Invalid operation.");
+            return;
+    }
+
+    document.getElementById("result").textContent = result;
+}
